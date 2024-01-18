2fad4acacb1b600b22f88fc660cffffed499d3d1
Lisa Liu <155706499+lisaqyliu@users.noreply.github.com>
2024-01-17 16:02:18 -0800

    Add files via upload

---
+++ IntegerExercise.java
@@ -0,0 +1,9 @@
+public class IntegerExercise {
+  public static void main(String[] args) {
+    int base = 4;
+    int exponent = 2;
+
+    int result = (int) Math.pow(base,exponent);
+    System.out.println(base + "raised to the power of" + exponent + "is" + result);
+  }
+}
